04c1c2a03feaef1eb6372fc6ed4501e8e6e58498
Mbelenga <mbelengaterence@gmail.com>
2024-07-03 06:36:18 -0700

    Dependancies

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,12 @@
-const weakMap = new WeakMap();
-const queryAPI = (endpoint) => {
-  const counter = (weakMap.get(endpoint) || 0) + 1;
-  if (counter >= 5) throw new Error('Endpoint load is high');
-  weakMap.set(endpoint, counter);
-};
-export { weakMap, queryAPI };
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
